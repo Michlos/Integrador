1e7a6a8fa9347757ff88d50124459a43e6e15490
Michlos MX <michlos@gmail.com>
2024-04-22 21:56:44 -0300

    #feature Primeira Migration

--- Domain/EmailConfigure/EmailConfigureModel.cs
+++ Domain/EmailConfigure/EmailConfigureModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Integrador.Domain.EmailConfigure
 {
+    [Table("EmailConfigure")]
     public class EmailConfigureModel : IEmailConfigureModel
     {
         [Key]
--- Services/AppDbContext.cs
+++ Services/AppDbContext.cs
@@ -12,11 +12,11 @@ namespace Integrador.Services
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<EmailConfigureModel> EmailConfigs { get; set; }
+        public DbSet<EmailConfigureModel> EmailConfigure { get; set; }                                                    
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=MIKE\\SQLEXPRESS;Initial Catalog=integradorDB;User ID=sa;Encrypt=False");
+            optionsBuilder.UseSqlServer("Data Source=MIKE\\SQLEXPRESS;Initial Catalog=integradorDB;User ID=sa;Password=By2@fi46;Encrypt=False");
         }
     }
 }
