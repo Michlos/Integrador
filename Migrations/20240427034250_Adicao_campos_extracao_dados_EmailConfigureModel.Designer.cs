3d9e05f02eafdb101ace4070fc5a21221950f2ef
Michlos MX <michlos@gmail.com>
2024-04-27 00:43:30 -0300

    #feature: migration de alteração do EmailConfigureModel

--- Migrations/20240427034250_Adicao_campos_extracao_dados_EmailConfigureModel.Designer.cs
+++ Migrations/20240427034250_Adicao_campos_extracao_dados_EmailConfigureModel.Designer.cs
@@ -4,14 +4,16 @@ using Integrador.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Integrador.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240427034250_Adicao_campos_extracao_dados_EmailConfigureModel")]
+    partial class Adicao_campos_extracao_dados_EmailConfigureModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
